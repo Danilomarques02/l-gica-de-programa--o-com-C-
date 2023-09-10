391ee6ac5402a1cbd8e1b346cd50c0ac4b277ef2
Danilo  Marques <125525104+Danilomarques02@users.noreply.github.com>
2023-09-10 16:51:13 -0300

    creste aula09

---
+++ Curso/aula09.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Globalization;
+
+namespace cursoc_007
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int vInt = 10;
+            short vShort = (short)vInt;
+
+            Console.WriteLine(vShort);
+            Console.ReadLine();
+        }
+
+    }
+}
