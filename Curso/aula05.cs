5eda682b8a7d2521e0e39b210f11907f1f4f53d5
Danilo  Marques <125525104+Danilomarques02@users.noreply.github.com>
2023-09-09 21:51:07 -0300

    aula05

---
+++ Curso/aula05.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace cursoc_007
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // & = AND / E 
+            // | = OR / OU 
+
+        
+            bool res = (5 > 3) & (10 < 5);
+
+            Console.WriteLine(res);
+            Console.ReadLine();
+        }
+    }
+}
