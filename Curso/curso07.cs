008f16c79b492e39664fbc30bc6e05766b8e16f9
Danilo  Marques <125525104+Danilomarques02@users.noreply.github.com>
2023-09-09 22:50:37 -0300

    create aula07

---
+++ Curso/curso07.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace cursoc_007
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            const string canal = "CFB Cursos";
+            const double pi= 3.1415;
+
+
+            Console.WriteLine("Canal: {0}\nP1: {1}", canal,pi);
+            Console.ReadLine();
+        }
+
+    }
+}
