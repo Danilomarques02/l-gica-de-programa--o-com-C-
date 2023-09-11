37288245d010a4d6d6004d18a80350ef8ce1155b
Danilo  Marques <125525104+Danilomarques02@users.noreply.github.com>
2023-09-11 20:49:18 -0300

    create aula12

---
+++ Curso/aula12.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Diagnostics.Eventing.Reader;
+using System.Globalization;
+
+namespace cursoc_007
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int n1, n2, n3, n4, res;
+            res=n1 = n2 = n3 = n4 = 0;
+
+            string resultado;
+
+
+            Console.Write("Digite a nota 1: ");
+            n1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite a nota 2: ");
+            n2 = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite a nota 3: ");
+            n3 = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite a nota 4: ");
+            n4 = int.Parse(Console.ReadLine());
+
+
+            res = n1 + n2 + n3 + n4;
+
+            //>=60 - Aprovado
+            //59 e 40  - Recuperação
+            //<40 - Reprovado
+
+
+
+            if (res >= 60)
+            {
+                if (res >= 90)
+                {
+                    if (res >= 99)
+                    {
+                        resultado = "Aprovado com êxito";
+                    }
+                    else { 
+
+                    resultado = "Aprovado com louvor";
+                }
+                }
+                else
+                {
+                    resultado = "Aprovado";
+                }
+            }
+
+           
+              else
+            {
+                if (res>= 40)
+                {
+                    resultado = "Recuperação";
+                }
+                else
+                {
+                    resultado = "Reprovado";
+                }
+            }
+            Console.WriteLine("Nota: {0} - Resultado:{1} ",res, resultado);
+            Console.ReadLine();
+                
+                }
+
+    }
+}
